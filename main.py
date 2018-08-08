d3d57bba7cd7ca2901748d0b322081f2df906173
Roman Melnychuk <jenikuse@gmail.com>
2018-08-08 16:32:06 +0300

    made class Person. Need method set_person() fix

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from urllib import urlopen
+import json
 
-import urllib, json
+url = 'https://randomuser.me/api/?results=100'
+
+def get_people(url):  # get data from url and parse
+
+    response = urlopen(url)
+    data = json.loads(response.read())
+    people = data['results']
+
+    return people  # [0,1, .., 99] -> dictionary
+
+people = get_people(url)
 
 class Person:
 
-    def __init__(self, count):
-        self.count = count
 
-    def get_people(self): #get data from url and parse
-
-        url = 'https://randomuser.me/api/?results=100'
-        response = urllib.urlopen(url)
-        data = json.loads(response.read())
-        people = data['results']
-
-        return people    #[0,1, .., 99] -> dictionary
-
-    people = get_people()
-
-    def set_person(self, people, count):
-
-        person = {}
-        person['name'] = people[count]['name']['first'] + " " + people[count]['name']['last'] #fullname
-        person['gender'] = people[count]['gender']
-        person['email'] = people[count]['email']
-        person['phone'] = people[count]['phone']
-        person['medium'] = people[count]['picture']['medium']
+    def __init__(self, person):
+        self.person = person
 
 
-instancelist = [Person(i) for i in range(100)]
-print instancelist[0]
+    def set_person(self, person):
+        person_dict = {}
+        person_dict['name'] = person['name']['first'] + " " + person['name']['last'] #fullname
+        person_dict['gender'] = person['gender']
+        person_dict['email'] = person['email']
+        person_dict['phone'] = person['phone']
+        person_dict['medium'] = person['picture']['medium']
+        #return person_dict
 
+        print(person_dict)
 
+instancelist = []
 
+for i in range(10):
+     card = Person(people[i])
+     card.set_person(people[i])
+     print (card)
 
-#def filter_people(people):
-#    d1 = people[0]['gender']
+#print instancelist
 
 
 
@@ -47,9 +51,7 @@ print instancelist[0]
 
 
 # from flask import Flask, redirect, url_for, request
-#r = request()
-#data = r.json()
-#print(data)
+
 '''app = Flask(__name__)
 @app.route("/")
 def hello():
